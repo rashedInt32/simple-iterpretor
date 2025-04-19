4384260f5deb8f88eed68336c6754af685cc09b4
rashed parvez <parvez08eee@gmail.com>
2025-04-19 09:59:17 +0600

    tokenizing

---
+++ token/token.go
@@ -0,0 +1,25 @@
+package token
+
+type TokenType string
+
+type token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLOR = ";"
+)
